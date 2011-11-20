070c568574fe276bafa646f8389d78a8b24500f8
Ashley Camba Garrido <ashwoods@gmail.com>
2011-11-21 00:42:29 +0100

    fixed django countries?

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ VERSION = open('VERSION').read().lstrip('version: ').rstrip('\n')
 
 #TODO: make this pypi compatible
 setup(name='django-generic-locations',
-      version = VERSION,
-      packages=find_packages(),
-      exclude_package_data={'rh2': ['bin/*.pyc']},
-      setup_requires = ["setuptools_git >= 0.3",],
-      requires = ['django-countries','django']
-
+    version = VERSION,
+    packages=find_packages(),
+    exclude_package_data={'rh2': ['bin/*.pyc']},
+    setup_requires = ["setuptools_git >= 0.3",],
+    requires = [
+        'django_countries',
+        'django'
+    ],
 )
